43df124ddaf167a088729c4dac837ffd87cc59f4
shoo <supershoo3@gmail.com>
2023-01-29 17:26:40 -0600

    finalized positioning of cart elements, still displays weird sometimes.

--- react-app/src/components/Cart/cart.js
+++ react-app/src/components/Cart/cart.js
@@ -4,10 +4,13 @@ import { useHistory, useParams } from "react-router-dom";
 import { createCart } from "../../store/cart";
 import { deleteCart } from "../../store/cart";
 import { getCart } from "../../store/cart";
+import './cart.css'
+import products from "../../store/product";
 
 function Cart(){
   const dispatch = useDispatch()
   const user = useSelector(state => state.session.user)
+  let total = 0
 
   const addtoCart = async (productId) => {
 
@@ -37,37 +40,55 @@ function Cart(){
   const carts = Object.values(cartsObj)
   const itemsArr = []
   const productsArr = []
+  console.log("all items", carts)
   carts.forEach(product => {
+    console.log(product)
     if (!itemsArr.includes(product.productId)){
       product.count = 1
-      itemsArr.push(product.productId)
-      productsArr.push(product)
+      itemsArr.unshift(product.productId)
+      productsArr.unshift(product)
     } else {
       productsArr.forEach(products => {
         if (products.productId === product.productId)
         products.count += 1
       })
     }
+    total += product.products.price
   })
 
 
 
   return (
-    <div>
+    <div className='container_cart'>
+      <div className='count_total'>
+        {carts.length === 1 ? <h1>{carts.length} Item in Cart</h1> : <h1>{carts.length} Items in Cart</h1>}
+      </div>
+      <div className="cart_items">
       {productsArr.map((product) => {
         return(
           <ul>
-            <li>
+            <li className='list_element'>
+            <div className='cart_details'>
+              <div>
               <img className='img'src={product.products.images[0].url}></img>
-              <span>{product.products.productName}</span>
+              </div>
+              <div className='amount'>
+              <p id="product_name">{product.products.productName}</p>
+              <button className='cart_button' onClick={e => addtoCart(product.productId)}>Add</button>
               <span> Amount: {product.count}</span>
+              <button className='cart_button' onClick={e => removefromCart(product.id)}>Remove</button>
+              </div>
+              </div>
+              <div className='price_details'>
               <span> Price: {product.count * product.products.price}</span>
-              <button onClick={e => addtoCart(product.productId)}>Add</button>
-              <button onClick={e => removefromCart(product.id)}>Remove</button>
+              <p>(each: {product.products.price})</p>
+              </div>
             </li>
           </ul>
         )
       })}
+      <h1 className='count_total'>total: {total}</h1>
+      </div>
     </div>
     )
   }
