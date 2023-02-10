3878ee81d0c6e4776ac08a6ebfff6cc42dacc331
shoo <supershoo3@gmail.com>
2023-02-09 18:03:18 -0600

    backend for reviews fully functioning(?)

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ from .api.auth_routes import auth_routes
 from .api.product_routes import product_routes
 from .api.image_routes import image_routes
 from .api.cart_routes import cart_routes
+from .api.review_routes import review_routes
 from .seeds import seed_commands
 from .config import Config
 
@@ -34,6 +35,7 @@ app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(product_routes, url_prefix='/api/products')
 app.register_blueprint(image_routes, url_prefix='/api/images')
 app.register_blueprint(cart_routes, url_prefix='/api/cart')
+app.register_blueprint(review_routes, url_prefix='/api/reviews')
 db.init_app(app)
 Migrate(app, db)
 
