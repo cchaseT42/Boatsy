19c259b1180842f1f6eb921197aa5b1dcaa8e80a
shoo <supershoo3@gmail.com>
2023-02-27 15:28:20 -0600

    delete order is working

--- react-app/src/components/SingleOrder/SingleOrder.js
+++ react-app/src/components/SingleOrder/SingleOrder.js
@@ -1,8 +1,9 @@
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useHistory, useParams } from 'react-router-dom'
-import { getOrder } from '../../store/orders'
+import { deleteOrder, getOrder } from '../../store/orders'
 import noimage from '../../no_image/No_Image_Available.jpg'
+import './SingleOrder.css'
 
 function SingleOrder() {
   const dispatch = useDispatch()
@@ -16,26 +17,43 @@ function SingleOrder() {
 
   console.log(orders)
 
+  const destroyOrder = async (id) => {
+    await dispatch(deleteOrder(id))
+    await history.push('/')
+  }
+
 
   useEffect(() => {
     if (user) dispatch(getOrder(orderId))
   }, [dispatch])
 
   return (
-    <div>
+    <div id="single_order">
       <div className="orderItems">
         {orderItems.map((item) => {
           return(
           <li key={item.id}>
+            <div className="order_item">
+              <div className="order_img">
             {item.products.images.length == 0 ? <img className='img' src={noimage}></img>:
             <img className='img' src={item.products.images[0].url}
               onError={(e)=>{ if (e.target.src !== noimage)
               { e.target.onerror = null; e.target.src=noimage; } }} alt='displayimg'></img>}
-              <p>{item.products.productDescription}</p>
+              </div>
+              <div className="item_details">
+              <p>Amount: {item.count}</p>
+              <p>{item.products.productName}</p>
+              <p>${item.products.price}</p>
+              </div>
+              </div>
           </li>
           )
         })}
       </div>
+      <div className="order_details">
+        <p className="order_price">{orders.total} Item(s): ${orders.subTotal}</p>
+        <button id='delete_order_button' onClick={e => destroyOrder(orders.id)}>Cancel Order</button>
+        </div>
     </div>
   )
 }
